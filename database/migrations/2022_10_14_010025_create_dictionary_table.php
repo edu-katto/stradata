344131155e527360aa93e7af1b1bc757fa02bb03
Andres Sanchez <adsanchez@uniguajira.edu.co>
2022-10-13 21:40:40 -0500

    init

---
+++ database/migrations/2022_10_14_010025_create_dictionary_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dictionary', function (Blueprint $table) {
+            $table->id();
+            $table->string('departamento');
+            $table->string('localidad');
+            $table->string('municipio');
+            $table->string('nombre');
+            $table->integer('ano_activo');
+            $table->string('tipo_persona');
+            $table->string('tipo_cargo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dictionary');
+    }
+};
